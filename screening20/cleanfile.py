4e6ab6537933eb83e8677e5c0a44c432bfefcb52
Andreas <andreas.helfenstein@hotmail.ch>
2016-03-09 15:42:39 +0200

    refactored code

--- screening20/cleanfile.py
+++ screening20/cleanfile.py
@@ -5,7 +5,8 @@ from collections import defaultdict
 
 include_categories = ['A', 'B', 'C', 'D', 'E']
 include_depth = len(include_categories[0])
-cat_file = "categories.csv"
+cat_file = "../categories.csv"
+res_file = "../results.txt"
 categories = defaultdict()
 new_lines = []
 
@@ -15,7 +16,10 @@ def find_category(query, idx=0):
     mesh_db = stem_url + "esearch.fcgi?db=%s&term=%s&retmode=json"
     record_db = stem_url + "esummary.fcgi?db=%s&id=%s&retmode=json"
     record_list = requests.get(mesh_db % (db, query)).json()
-    mesh_id = record_list['esearchresult']['idlist'][idx]
+    try:
+        mesh_id = record_list['esearchresult']['idlist'][idx]
+    except KeyError:
+        print record_list
     record_data = requests.get(record_db % (db, mesh_id)).json()
     treenum = record_data['result'][mesh_id]['ds_idxlinks'][idx]['treenum']
     meshterm = record_data['result'][mesh_id]['ds_meshterms'][idx]
@@ -25,32 +29,40 @@ def find_category(query, idx=0):
 
 try:
     with open(cat_file, 'r') as f:
-        lines = f.readlines()
-    for line in lines:
-        term, cat = line.rstrip('\n').split(',')
-        categories[term] = cat
+        for line in f:
+
+    #    lines = f.readlines()
+    #for line in lines:
+            term, cat = line.rstrip('\n').split(',')
+            categories[term] = cat
 except IOError:
     pass
     
-with open('resfile.txt', 'r') as f:
-    lines = f.readlines()
-for line in lines:
-    new_line = []
-    term_list = line.rstrip('\n').split(',')
-    for term in term_list:
-        if term in categories.keys():
-            cat = categories[term]
-        else:
-            try:
-                t, cat = find_category(term)
-            except ConnectionError:
-                cat = None
-            categories[term] = cat
-        if cat[0:include_depth] in include_categories:
-            new_line.append(term)
-    new_lines.append(new_line)
+with open(res_file, 'r') as f:
+#    lines = f.readlines()
+#for line in lines:
+    for line in f:
+        if line.startswith('#'):
+            continue
+        if line == '\n':
+            continue
+        new_line = []
+        term_list = line.rstrip('\n').split(',')
+        for term in term_list:
+            print term
+            if term in categories.keys():
+                cat = categories[term]
+            else:
+                try:
+                    t, cat = find_category(term)
+                except requests.ConnectionError:
+                    cat = None
+                categories[term] = cat
+            if cat[0:include_depth] in include_categories:
+                new_line.append(term)
+        new_lines.append(new_line)
     
-with open('resfile_n.txt', 'w') as f:
+with open(res_file + '_clean.txt', 'w') as f:
     for l1 in new_lines:
         f.write(','.join(l1))
         f.write('\n')
--- screening20/correlate.py
+++ screening20/correlate.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 
-import pandas
+import numpy as np
+import scipy
 from collections import defaultdict
 import itertools
 import networkx as nx
@@ -25,67 +26,103 @@ def generate_edges(col, graph):
             graph.add_edge(start, end, attr_dict={'weight':weight})
     return col
 
-with open(cat_file, 'r') as f:
-    lines = f.readlines()
-for line in lines:
-    term, cat = line.rstrip('\n').split(',')
-    categories[term] = cat
-    
-with open('resfile_n.txt', 'r') as f:
-    lines = f.readlines()
-for line in lines:
-    line = line.rstrip('\n')
-    term_list = line.split(',')
-    combs = itertools.combinations(term_list, 2)
-    for x,y in combs:
-        try:
-            corr_map[x][y] += 1
-        except KeyError:
-            corr_map[x][y] = 1
-df = pandas.DataFrame.from_dict(corr_map)
-df.fillna(0, inplace=True)
+def _is_in_cat(cat, all_cats):
+    if not all_cats:
+        return True
+    for single_cat in all_cats:
+        if cat.startswith(single_cat):
+            return True
+    return False
+
+def _filter_terms(res_file, cat_file, categories):
+    """return results from res_file that fall into categories."""
+
+    with open(cat_file, 'r') as cf:
+        cf_pairs = [line.split(',') for line in cf]
+        cat_table = {term.strip(): code.strip() for term, code in cf_pairs}
+
+    with open(res_file, 'r') as rf:
+        rf_pairs = [line.split(',') for line in rf]
+        filtered_res = [[term.strip() for term in terms if
+            _is_in_cat(cat_table[term.strip()], categories)] for terms in rf_pairs]
+
+    return filtered_res, cat_table
+
+def build_matrix(res_file, cat_file, categories=[]):
+    """Build sparse x*x matrix for correlation"""
+
+    active_terms, cat_table = _filter_terms(res_file, cat_file, categories)
+    active_terms_flat = itertools.chain.from_iterable(active_terms) 
+    active_set = list(set(active_terms_flat))
+    dim = len(active_set)
+    lookup = {term: idx for idx, term in enumerate(active_set)}
+    rev_lookup = {idx: term for idx, term in enumerate(active_set)}
+    corr_map = scipy.sparse.dok_matrix((dim, dim), dtype=np.int)
+
+    for active_terms_line in active_terms:
+        combs = itertools.combinations(active_terms_line, 2)
+        for x,y in combs:
+            corr_map[lookup[x], lookup[y]] += 1
+    mappings = {'lookup': lookup, 'rev_lookup': rev_lookup, 'cat_table': cat_table}
+    return corr_map, mappings
+
+def create_plot(corr_map, mappings, minweight=1):
+
+    all_terms = lookup.values()
+    #G=nx.random_geometric_graph(200,0.125)
+    G = nx.MultiGraph()
+    for i in all_terms:
+        G.add_node(i)
+    for item in corr_map.items():
+        start, end = item[0]
+        weight = item[1]
+        if weight >= minweight:
+            G.add_edge(start, end, attr_dict={'weight':weight})
+#    colorscheme = [color_by_cat[categories[t][0:1]] for t in all_terms]
+#    print colorscheme
 
 
-all_terms = df.index
-#http://eutils.ncbi.nlm.nih.gov/entrez/eutils/
-#G=nx.random_geometric_graph(200,0.125)
-G = nx.MultiGraph()
-for i in all_terms:
-    G.add_node(i)
-colorscheme = [color_by_cat[categories[t][0:1]] for t in all_terms]
-print colorscheme
-df.apply(generate_edges, axis=0, args =(G,))
+    # position is stored as node attribute data for random_geometric_graph
+    #pos=nx.get_node_attributes(G,'pos')
 
-# position is stored as node attribute data for random_geometric_graph
-#pos=nx.get_node_attributes(G,'pos')
+    # find node near center (0.5,0.5)
+    #dmin=1
+    #ncenter=0
+    #for n in pos:
+    #    x,y=pos[n]
+    #    d=(x-0.5)**2+(y-0.5)**2
+    #    if d<dmin:
+    #        ncenter=n
+    #        dmin=d
 
-# find node near center (0.5,0.5)
-#dmin=1
-#ncenter=0
-#for n in pos:
-#    x,y=pos[n]
-#    d=(x-0.5)**2+(y-0.5)**2
-#    if d<dmin:
-#        ncenter=n
-#        dmin=d
+    # color by path length from node near center
+    #p=nx.single_source_shortest_path_length(G,ncenter)
 
-# color by path length from node near center
-#p=nx.single_source_shortest_path_length(G,ncenter)
+    plt.figure(figsize=(8,8))
+    node_labels = {i: mappings[rev_lookup][i] for i in all_terms}
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    #nx.draw(G, pos)
 
-plt.figure(figsize=(8,8))
-node_labels = {i: i for i in all_terms}
-pos = nx.spring_layout(G)
-nx.draw_networkx_labels(G, pos, labels=node_labels)
-#nx.draw(G, pos)
+    nx.draw_networkx_edges(G,pos,alpha=0.4)
+    nx.draw_networkx_nodes(G,pos,
+                           node_size=80,
+                           #node_color=colorscheme,
+                           cmap=plt.cm.Reds_r)
 
-nx.draw_networkx_edges(G,pos,alpha=0.4)
-nx.draw_networkx_nodes(G,pos,
-                       node_size=80,
-                       node_color=colorscheme,
-                       cmap=plt.cm.Reds_r)
+    #plt.xlim(-0.05,1.05)
+    #plt.ylim(-0.05,1.05)
+    plt.axis('off')
+    return plt
 
-#plt.xlim(-0.05,1.05)
-#plt.ylim(-0.05,1.05)
-plt.axis('off')
-plt.savefig('graph.png')
-plt.show()
+if __name__ == '__main__':
+    categories = ('A', 'B')
+    corr_map, mappings = build_matrix('resfile_n.txt',
+        'categories.csv', categories=categories)
+    plt = create_plot(corr_map, mappings)
+    plt.show()
+#    print corr_map.todense()
+
+def blah():
+    plt.savefig('graph.png')
+    plt.show()
