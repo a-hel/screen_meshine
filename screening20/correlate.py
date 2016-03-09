d76268be6c9956c35a135b04587e76db875f6dff
Andreas <andreas.helfenstein@hotmail.ch>
2016-03-09 22:08:36 +0200

    massive speedup

--- screening20/correlate.py
+++ screening20/correlate.py
@@ -10,13 +10,15 @@ import matplotlib.pyplot as plt
 corr_map = defaultdict(dict)
 categories = defaultdict()
 cat_file = "categories.csv"
-color_by_cat = {'A': 'red',
-    'B': 'blue',
-    'C': 'green',
-    'D': 'orange',
-    'E': 'yellow',
-    'F': 'black',
-    'G': 'white'}
+
+def color_by_cat(mappings, all_terms):
+    colors = {'A': 'red',
+        'B': 'blue',
+        'C': 'green',
+        'D': 'orange',
+        'E': 'yellow',
+        'F': 'black',
+        'G': 'white'}
 
 def generate_edges(col, graph):
     start = col.name
@@ -38,11 +40,11 @@ def _filter_terms(res_file, cat_file, categories):
     """return results from res_file that fall into categories."""
 
     with open(cat_file, 'r') as cf:
-        cf_pairs = [line.split(',') for line in cf]
+        cf_pairs = [line.split(',') for line in cf if line != '\n']
         cat_table = {term.strip(): code.strip() for term, code in cf_pairs}
 
     with open(res_file, 'r') as rf:
-        rf_pairs = [line.split(',') for line in rf]
+        rf_pairs = [line.split(',') for line in rf if line != '\n']
         filtered_res = [[term.strip() for term in terms if
             _is_in_cat(cat_table[term.strip()], categories)] for terms in rf_pairs]
 
@@ -63,52 +65,63 @@ def build_matrix(res_file, cat_file, categories=[]):
         combs = itertools.combinations(active_terms_line, 2)
         for x,y in combs:
             corr_map[lookup[x], lookup[y]] += 1
-    mappings = {'lookup': lookup, 'rev_lookup': rev_lookup, 'cat_table': cat_table}
+    mappings = {'lookup': lookup, 'rev_lookup': rev_lookup, 'cat_table': cat_table}
+    corr_map = corr_map + corr_map.transpose()
     return corr_map, mappings
 
 def create_plot(corr_map, mappings, minweight=1):
 
-    all_terms = lookup.values()
+    node_scale = 1
+    node_sums = corr_map.sum(axis=0).tolist()[0]
+
+
+    all_terms = mappings['lookup'].values()
+
+    edgewidth = []
+    nodeprops = []
     #G=nx.random_geometric_graph(200,0.125)
     G = nx.MultiGraph()
-    for i in all_terms:
-        G.add_node(i)
+
+    corr_map_coo = corr_map.tocoo()
+    max_correlations = corr_map_coo.max(axis=0).toarray()[0]
+    node_sums = corr_map_coo.sum(axis=0).tolist()[0]
+
+    for i, max_ in enumerate(max_correlations):
+        if max_ >= minweight:
+            G.add_node(i)
+            n_color = 'green'
+            n_size =  node_sums[i]*node_scale
+            n_label = mappings['rev_lookup'][i]
+            nodeprops.append([n_color, n_size, n_label, i])
+            #nodelabels.append(mappings['rev_lookup'][i])
+    nodecolor, nodesize, nodelabels, idx = zip(*nodeprops)
+
     for item in corr_map.items():
         start, end = item[0]
         weight = item[1]
         if weight >= minweight:
-            G.add_edge(start, end, attr_dict={'weight':weight})
-#    colorscheme = [color_by_cat[categories[t][0:1]] for t in all_terms]
-#    print colorscheme
+            print "Drawing edge: %s -> %s (Weight: %s)" % (mappings['rev_lookup'][start],
+                    mappings['rev_lookup'][end], weight)
+            edgewidth.append(weight)
+            G.add_edge(start, end)
 
+    edgewidth = np.array(edgewidth, dtype='uint8')
+    edgewidth = np.around(np.log(edgewidth))
+
+    nodesize = np.array(nodesize, dtype='uint8')*4
+    nodelabels_dict = {idx[i]: lbl for i, lbl in enumerate(nodelabels)}
 
     # position is stored as node attribute data for random_geometric_graph
     #pos=nx.get_node_attributes(G,'pos')
 
-    # find node near center (0.5,0.5)
-    #dmin=1
-    #ncenter=0
-    #for n in pos:
-    #    x,y=pos[n]
-    #    d=(x-0.5)**2+(y-0.5)**2
-    #    if d<dmin:
-    #        ncenter=n
-    #        dmin=d
 
-    # color by path length from node near center
-    #p=nx.single_source_shortest_path_length(G,ncenter)
+    plt.figure(figsize=(50,50))
+    #pos = nx.spring_layout(G)
+    pos = nx.circular_layout(G)
 
-    plt.figure(figsize=(8,8))
-    node_labels = {i: mappings[rev_lookup][i] for i in all_terms}
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_labels(G, pos, labels=node_labels)
-    #nx.draw(G, pos)
-
-    nx.draw_networkx_edges(G,pos,alpha=0.4)
-    nx.draw_networkx_nodes(G,pos,
-                           node_size=80,
-                           #node_color=colorscheme,
-                           cmap=plt.cm.Reds_r)
+    nx.draw_networkx_labels(G, pos, labels=nodelabels_dict, fontsize=12)
+    nx.draw_networkx_edges(G,pos, alpha=0.6, width=edgewidth, edge_color='grey')
+    nx.draw_networkx_nodes(G,pos, node_size=nodesize, node_color=nodecolor, alpha=0.6)
 
     #plt.xlim(-0.05,1.05)
     #plt.ylim(-0.05,1.05)
@@ -116,10 +129,14 @@ def create_plot(corr_map, mappings, minweight=1):
     return plt
 
 if __name__ == '__main__':
-    categories = ('A', 'B')
-    corr_map, mappings = build_matrix('resfile_n.txt',
-        'categories.csv', categories=categories)
-    plt = create_plot(corr_map, mappings)
+    categories = ('A', # Anatomy
+        'C', # Diseases
+        'B', # Organsims
+        'R', # Pharmacological actions
+        'J') # Technology, food, beverages
+    corr_map, mappings = build_matrix('../results.txt_clean.txt',
+        '../categories.csv', categories=categories)
+    plt = create_plot(corr_map, mappings, minweight=20)
     plt.show()
 #    print corr_map.todense()
 
