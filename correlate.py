b421fc1dfdca66d2c44449ca84637d35164413a8
Andreas <andreas.helfenstein@hotmail.ch>
2016-05-23 19:55:34 +0300

    integrated correlate into workflow

--- correlate.py
+++ correlate.py
@@ -1,5 +1,6 @@
 #!usr/bin/python
 
+import sys
 import numpy as np
 import scipy
 from collections import defaultdict
@@ -7,30 +8,31 @@ import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
 
-corr_map = defaultdict(dict)
-categories = defaultdict()
-cat_file = "categories.csv"
 
-def color_by_cat(mappings, all_terms):
-    colors = {'A': 'red',
+def _get_colors(cats, col_scheme="default"):
+    default = {'A': 'red',
         'B': 'blue',
         'C': 'green',
         'D': 'orange',
         'E': 'yellow',
         'F': 'black',
         'G': 'white'}
+    all_schemes = {"default": default}
+    cur_scheme = all_schemes[col_scheme]
+    colors = [cur_scheme[cat[0]] for cat in cats]
+    return colors
 
 def _is_in_cat(cat, all_cats):
     """Check if cat is in all_cats."""
 
-    if not all_cats:
+    if all_cats == []:
         return True
     for single_cat in all_cats:
         if cat.startswith(single_cat):
             return True
     return False
 
-def _filter_terms(res_file, cat_file, categories):
+def _filter_termsxx(res_file, cat_file, categories):
     """return results from res_file that fall into categories."""
 
     with open(cat_file, 'r') as cf:
@@ -42,22 +44,55 @@ def _filter_terms(res_file, cat_file, categories):
             _is_in_cat(cat_table[term.strip()], categories)] for terms in rf_pairs]
     return filtered_res, cat_table
 
-def build_matrix(res_file, cat_file, categories=[]):
+def _load_dataset(res_file, categories, exclude=[]):
+    posts = {}
+    with open(res_file, "r") as f:
+        for line in f:
+            if line.isspace():
+                continue
+            elif line.startswith('#'):
+                cur_key = line[line.rfind("_")+1:line.rfind(".")]
+                posts[cur_key] = []
+            else:
+                data = line.split("|")
+                mesh = data[1].lstrip("*")
+                if mesh in exclude:
+                    continue
+                cat = data[2]
+                if _is_in_cat(cat, categories):
+                    posts[cur_key].append((mesh, cat))
+    return posts
+
+def _flatten(dataset):
+    arr = reduce(list.__add__,[dataset[key] for key in dataset.keys()])
+    uniques = list(set(arr))
+    terms, cats = zip(*uniques)
+    colors = _get_colors(cats, col_scheme="default")
+    return terms, cats, colors
+
+
+def build_matrix(res_file, categories=[], highlight=False, exclude=[],
+    color_scheme="default"):
     """Build sparse x*x matrix for correlation"""
 
-    active_terms, cat_table = _filter_terms(res_file, cat_file, categories)
-    active_terms_flat = itertools.chain.from_iterable(active_terms)
-    active_set = list(set(active_terms_flat))
-    dim = len(active_set)
-    lookup = {term: idx for idx, term in enumerate(active_set)}
-    rev_lookup = {idx: term for idx, term in enumerate(active_set)}
+    dataset = _load_dataset(res_file, categories, exclude)
+    terms, cats, colors = _flatten(dataset)
+    dim = len(terms)
+    lookup = {term: idx for idx, term in enumerate(terms)}
     corr_map = scipy.sparse.dok_matrix((dim, dim), dtype=np.int)
 
-    for active_terms_line in active_terms:
-        combs = itertools.combinations(active_terms_line, 2)
+    for keys in dataset:
+        termline = zip(*dataset[keys])[0]
+        if not highlight:
+            combs = itertools.combinations(termline, 2)
+        else:
+            if highlight not in termline:
+                continue
+            else:
+                combs = ((highlight, term) for term in termline if term != highlight)
         for x,y in combs:
             corr_map[lookup[x], lookup[y]] += 1
-    mappings = {'lookup': lookup, 'rev_lookup': rev_lookup, 'cat_table': cat_table}
+    mappings = {'lookup': lookup, 'terms': terms, 'cats': cats, 'colors': colors}
     corr_map = corr_map + corr_map.transpose()
     return corr_map, mappings
 
@@ -66,9 +101,10 @@ def create_plot(corr_map, mappings, minweight=1):
     node_scale = 4
     edge_scale = 1
     node_sums = corr_map.sum(axis=0).tolist()[0]
-    all_terms = mappings['lookup'].values()
+    terms = mappings['terms']
     edgewidth = []
     nodeprops = []
+    lit_edges = []
     G = nx.MultiGraph()
     corr_map_coo = corr_map.tocoo()
     max_correlations = corr_map_coo.max(axis=0).toarray()[0]
@@ -76,17 +112,17 @@ def create_plot(corr_map, mappings, minweight=1):
     for i, max_ in enumerate(max_correlations):
         if max_ >= minweight:
             G.add_node(i)
-            n_color = 'green'
+            n_color = mappings['colors'][i]
+            #n_color = 'Green'
             n_size =  node_sums[i]*node_scale
-            n_label = mappings['rev_lookup'][i]
+            n_label = terms[i]
             nodeprops.append([n_color, n_size, n_label, i])
     nodecolor, nodesize, nodelabels, idx = zip(*nodeprops)
     for item in corr_map.items():
         start, end = item[0]
         weight = item[1]
         if weight >= minweight:
-            print "Drawing edge: %s -> %s (Weight: %s)" % (mappings['rev_lookup'][start],
-                    mappings['rev_lookup'][end], weight)
+            lit_edges.append((terms[start], terms[end], str(weight)))
             edgewidth.append(weight)
             G.add_edge(start, end)
     edgewidth = np.array(edgewidth, dtype='uint8')
@@ -103,16 +139,59 @@ def create_plot(corr_map, mappings, minweight=1):
     #plt.xlim(-0.05,1.05)
     #plt.ylim(-0.05,1.05)
     plt.axis('off')
-    return plt
+    return plt, lit_edges
 
+def usage():
+    return "blah"
 if __name__ == '__main__':
-    categories = ('A', # Anatomy
-        'C', # Diseases
-        'B', # Organsims
-        'R', # Pharmacological actions
-        'J') # Technology, food, beverages
-    corr_map, mappings = build_matrix('../results.txt_clean.txt',
-        '../categories.csv', categories=categories)
-    plt = create_plot(corr_map, mappings, minweight=20)
+    #exclude, minweight
+    categories = []
+    minweight = 10
+    exclude = []
+    highlight = False
+    color_scheme = "default"
+    gid = 1
+    if len(sys.argv) <= 1:
+        print(usage())
+        sys.exit(0)
+    else:
+        i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if arg.startswith('-'):
+            if arg in ('-h', '--help'):
+                print(usage())
+                sys.exit(0)
+            if arg in ('-mw', '--minweight'):
+                i += 1
+                minweight = int(sys.argv[i])
+            if arg in ('-hl', '--highlight'):
+                i += 1
+                highlight = sys.argv[i]
+            if arg in ('-c', '--colors'):
+                i += 1
+                color_scheme = sys.argv[i]
+            if arg in ('-p', '--project'):
+                i += 1
+                project = sys.argv[i]
+                    
+        elif arg.startswith('!'):
+            exclude.append(arg[1:])
+        else:
+            categories.append(arg)
+        i += 1
+    project_path = "../projects/"
+    corr_map, mappings = build_matrix(project_path + project + "/out.txt",
+        categories=categories, highlight=highlight,  exclude=exclude,
+        color_scheme=color_scheme)
+    plt, edges = create_plot(corr_map, mappings, minweight=minweight)
+    with open(project_path + project +'/info_%s.txt' % gid, 'w') as f:
+        f.write('# Information for Graph %s\n' % gid)
+        f.write('# Project: %s\n' % project)
+        f.write('# Command: %s\n' % " ".join(sys.argv))
+        f.write('# Edges (start, end, weight):\n')
+        for edge in edges:
+            f.write(", ".join(edge))
+            f.write("\n")
+    plt.savefig(project_path + project +'/graph_%s.png' % gid)
     plt.show()
-    #plt.savefig('graph.png')
